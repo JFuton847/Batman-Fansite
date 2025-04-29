dad9d8a0ef84812c4aea5f8755fa92f64778ca1b
JFuton847 <jimmyfuton847@gmail.com>
2025-04-29 12:47:16 -0500

    2nd commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__content">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "../Header/Header.css";
+
+<header className="header">
+  <div className="header__container">
+    <h1 className="header__name">Batman Fansite</h1>
+  </div>
+</header>;
+
+export default Header;
