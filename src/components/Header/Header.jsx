51d80c3b4def29bee34c1a469bb540697a71a508
JFuton847 <jimmyfuton847@gmail.com>
2025-05-01 12:20:44 -0500

    start work on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import "../Header/Header.css";
 
-<header className="header">
-  <div className="header__container">
-    <h1 className="header__name">Batman Fansite</h1>
-  </div>
-</header>;
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <h1 className="header__name">Gotham Gazette</h1>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
